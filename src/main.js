110cc18bae9e91d4f3e1b9d1afc91cc30b68c5ee
pearpages <pere@soms.cat>
2017-03-21 15:35:05 +0100

    close menu

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 (function(){
     var burger = document.querySelector('.burger-container'),
         header = document.querySelector('.header');
+    var links = document.querySelectorAll('.menu-item a');
 
     burger.onclick = function() {
         header.classList.toggle('menu-opened');
     }
+    links.forEach(function (x) {
+        x.onclick = function () {
+            header.classList.toggle('menu-opened');
+        }
+    });
 }());
